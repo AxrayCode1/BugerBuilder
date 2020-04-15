885bb22b015345e373505384201c465452cc09ec
SHIHCHANG <axray@hotmail.com>
2020-04-16 00:00:27 +0800

    completed contact-data

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
+
+import axios from '../../../axios-orders';
+
+class ContactData extends Component{
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        },
+        loading: false        
+    }
+
+    orderHandler = event  => {                 
+        event.preventDefault();              
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Max',
+                address:{
+                    street: 'Teststreet 1',
+                    zipCode: '334',
+                    country: 'Taiwain'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }        
+        axios.post('/orders.json',order)
+            .then(response => {
+                this.setState({loading: false});
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({loading: false});
+            });
+    }
+    
+
+    render(){
+        return(
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                <form>
+                    <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                    <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
+                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                    <Button clicked={this.orderHandler} btnType="Success" >ORDER</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ContactData;
